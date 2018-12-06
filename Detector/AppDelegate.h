29b6c903725ce0d44f8213f5d992a2d620a5465e
CNOOOOO <15216656514@163.com>
2018-12-06 12:11:48 +0800

    Initial Commit

---
+++ Detector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Detector
+//
+//  Created by Mac1 on 2018/12/6.
+//  Copyright © 2018 Mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
